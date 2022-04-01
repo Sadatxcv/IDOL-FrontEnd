000cf55691eed3aca7283c85e73836a669a5e068
Sadatxcv <sadatxcv@gmail.com>
2022-04-02 00:33:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
