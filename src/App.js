574da09b3e3e2cf115fbaa1217792810cccd30fe
Sadatxcv <sadatxcv@gmail.com>
2022-04-03 16:32:46 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Component/Pages/Home/Home';
+import LoginAndSignup from './Component/Pages/LoginAndSignup/LoginAndSignup';
+import Navbar from "./Component/Navbar/Navbar"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar></Navbar>
       <Routes>
-        
+      <Route exact path="/" element={<Home />}/>
+      <Route exact path="/LoginAndSignup" element={<LoginAndSignup />}/>
       </Routes>
       </BrowserRouter>
     </div>
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import "./Navbar.css";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Header = () => {
+  return (
+    <div className="navbar-container">
+      <Navbar collapseOnSelect className="nav" expand="lg">
+        <Container>
+          <Navbar.Brand href="/home">
+            <Link to="/home">
+              <img className="img-logo" src="/static/logo.png" alt="logo"></img>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Item>
+                <Nav.Link
+                  className="navItem"
+                  id="navlink"
+                  eventKey="1"
+                  as={Link}
+                  to="/"
+                >
+                  Home
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  className="navItem"
+                  id="navlink"
+                  eventKey="1"
+                  as={Link}
+                  to="/LoginAndSignup"
+                >
+                  Login & Sign Up
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
-export default Navbar;
+export default Header;
