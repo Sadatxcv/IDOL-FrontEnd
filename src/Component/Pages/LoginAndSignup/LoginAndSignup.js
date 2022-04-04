d8debbcadb5ada8db32e1bf0aa51c41b10f552fd
Sadatxcv <sadatxcv@gmail.com>
2022-04-04 14:25:31 +0600

    loginsignup page

--- src/Component/Pages/LoginAndSignup/LoginAndSignup.js
+++ src/Component/Pages/LoginAndSignup/LoginAndSignup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import "./LoginAndSignup.css";
 
 const LoginAndSignup = () => {
@@ -17,12 +18,11 @@ const LoginAndSignup = () => {
   });
 
   return (
-    <div>
-      <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
-      <div className="container" id="container">
+    <div className="LoginAndSignup-body">
+      <Container className="LoginAndSignup-container" id="container">
         <div className="form-container sign-up-container">
-          <form action="#">
-            <h1>Create Account</h1>
+          <form className="LoginAndSignup-form" action="#">
+            <h1 className="LoginAndSignup-h1">Create Account</h1>
             <div className="social-container">
               <a href="#" className="social">
                 <i className="fab fa-facebook-f"></i>
@@ -34,16 +34,16 @@ const LoginAndSignup = () => {
                 <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
+            <span className="LoginAndSignup-span">or use your email for registration</span>
+            <input className="LoginAndSignup-input" type="text" placeholder="Name" />
+            <input className="LoginAndSignup-input" type="email" placeholder="Email" />
+            <input className="LoginAndSignup-input" type="password" placeholder="Password" />
+            <button className="LoginAndSignup-button">Sign Up</button>
           </form>
         </div>
         <div className="form-container sign-in-container">
-          <form action="#">
-            <h1>Sign in</h1>
+          <form className="LoginAndSignup-form" action="#">
+            <h1 className="LoginAndSignup-h1">Sign in</h1>
             <div className="social-container">
               <a href="#" className="social">
                 <i className="fab fa-facebook-f"></i>
@@ -55,34 +55,34 @@ const LoginAndSignup = () => {
                 <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
-            <span>or use your account</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
+            <span className="LoginAndSignup-span">or use your account</span>
+            <input className="LoginAndSignup-input" type="email" placeholder="Email" />
+            <input className="LoginAndSignup-input" type="password" placeholder="Password" />
+            <a className="LoginAndSignup-forgotpass" href="#">Forgot your password?</a>
+            <button className="LoginAndSignup-button">Sign In</button>
           </form>
         </div>
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
+              <h1 className="LoginAndSignup-h1">Welcome Back!</h1>
               <p>
                 To keep connected with us please login with your personal info
               </p>
-              <button className="ghost" id="signIn">
+              <button className="LoginAndSignup-button ghost" id="signIn">
                 Sign In
               </button>
             </div>
             <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
+              <h1 className="LoginAndSignup-h1">Hello, Friend!</h1>
               <p>Enter your personal details and start journey with us</p>
-              <button className="ghost" id="signUp">
+              <button className="LoginAndSignup-button ghost" id="signUp">
                 Sign Up
               </button>
             </div>
           </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
