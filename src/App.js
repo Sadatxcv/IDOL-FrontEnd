b1f7139b627a4587317c2d50be8e8fe592e042e8
Shahidul Islam Noman <1730915@iub.edu.bd>
2022-04-22 17:01:16 +0600

    All pages (themantic & discussion) Done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Navbar from "./Component/Navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LangForm from "./Component/Forms/LangForm";
 import MindMapDiscussion from "./Component/Pages/MindMap/MindMapDiscussion";
+import ThemanticAnalysis from "./Component/Pages/ThemanticAnalysis/ThemanticAnalysis";
+import Discussion from "./Component/Pages/Discussion/Discussion";
+import About from "./Component/Pages/About/About";
+import Contact from "./Component/Pages/Contact/Contact";
+
 
 function App() {
   return (
@@ -20,7 +25,12 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/LoginAndSignup" element={<LoginAndSignup />} />
           <Route exact path="/langform" element={<LangForm />} />
-          <Route exact path="/MindMapDiscussion" element={<MindMapDiscussion/>}/>
+          <Route exact path="/mindMapDiscussion" element={<MindMapDiscussion/>}/>
+          <Route exact path="/themanticAnalysis" element={<ThemanticAnalysis/>}/>
+          <Route exact path="/discussion" element={<Discussion/>}/>
+          <Route exact path="/About" element={<About/>}/>
+          <Route exact path="/Contact" element={<Contact/>}/>
+          Contact
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Component/Pages/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div>
+      <Container className="About-Contact">
+        <h1>This is the contact page.</h1> 
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
